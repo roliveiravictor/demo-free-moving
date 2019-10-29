db2d7da9102ded29525482bbd9a70af2692f5cbc
Victor R <victor.rocha@mercadolivre.com>
2019-10-29 05:14:18 -0300

    removed unecessary call from navigator activity

--- app/src/main/java/com/stonetree/freemoving/NavigatorActivity.kt
+++ app/src/main/java/com/stonetree/freemoving/NavigatorActivity.kt
@@ -9,6 +9,5 @@ class NavigatorActivity : MainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_main)
-        findNavController(R.id.navigator)
     }
 }
